e92a57aba40bb11fc2afb4c784885f72ee306af2
Waqas Tariq <waqas.tariq@goodlogics.com>
2018-03-12 12:43:09 +0500

    first commit

---
+++ WebApplication1/Migrations/201803031244345_InitialCreate.cs
@@ -0,0 +1,60 @@
+namespace WebApplication1.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Course",
+                c => new
+                    {
+                        CourseID = c.Int(nullable: false),
+                        Title = c.String(),
+                        Credits = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.CourseID);
+            
+            CreateTable(
+                "dbo.Enrollment",
+                c => new
+                    {
+                        EnrollmentID = c.Int(nullable: false, identity: true),
+                        CourseID = c.Int(nullable: false),
+                        StudentID = c.Int(nullable: false),
+                        Grade = c.Int(),
+                    })
+                .PrimaryKey(t => t.EnrollmentID)
+                .ForeignKey("dbo.Course", t => t.CourseID, cascadeDelete: true)
+                .ForeignKey("dbo.studentdetails", t => t.StudentID, cascadeDelete: true)
+                .Index(t => t.CourseID)
+                .Index(t => t.StudentID);
+            
+            CreateTable(
+                "dbo.studentdetails",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        LastName = c.String(),
+                        FirstMidName = c.String(),
+                        EnrollmentDate = c.DateTime(nullable: false),
+                        Secret = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Enrollment", "StudentID", "dbo.studentdetails");
+            DropForeignKey("dbo.Enrollment", "CourseID", "dbo.Course");
+            DropIndex("dbo.Enrollment", new[] { "StudentID" });
+            DropIndex("dbo.Enrollment", new[] { "CourseID" });
+            DropTable("dbo.studentdetails");
+            DropTable("dbo.Enrollment");
+            DropTable("dbo.Course");
+        }
+    }
+}
